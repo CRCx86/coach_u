8b609a97446e7225f9a79b3f1bc505846229569d
CRCx86 <crc32zinov@gmail.com>
2018-03-09 22:58:08 +0300

    commit all main files

---
+++ src/main/java/com/coachu/service/UserService.java
@@ -0,0 +1,29 @@
+package com.coachu.service;
+
+
+import com.coachu.model.User;
+import com.coachu.to.UserTo;
+import com.coachu.util.exception.NotFoundException;
+
+import java.util.List;
+
+public interface UserService {
+
+    User create(User user);
+
+    void delete(int id) throws NotFoundException;
+
+    User get(int id) throws NotFoundException;
+
+    User getByEmail(String email) throws NotFoundException;
+
+    void update(User user);
+
+    void update(UserTo user);
+
+    List<User> getAll();
+
+    void enable(int id, boolean enable);
+
+    User getWithMeals(int id);
+}
---
+++ src/main/java/com/coachu/web/user/ProfileRestController.java
@@ -0,0 +1,35 @@
+package com.coachu.web.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import com.coachu.AuthorizedUser;
+import com.coachu.model.User;
+import com.coachu.to.UserTo;
+
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get() {
+        return super.get(AuthorizedUser.id());
+    }
+
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete() {
+        super.delete(AuthorizedUser.id());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody UserTo userTo) {
+        super.update(userTo, AuthorizedUser.id());
+    }
+
+    @GetMapping(value = "/text")
+    public String testUTF() {
+        return "Русский текст";
+    }
+}
